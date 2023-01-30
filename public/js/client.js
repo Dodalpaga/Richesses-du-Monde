a0c34682b60afd3fa5f4406157676e6c7688f316
Dorian VOYDIE <dorian.voydie@gmail.com>
2023-01-30 20:34:17 +0100

    Update

--- public/js/client.js
+++ public/js/client.js
@@ -97,22 +97,33 @@ function outputPlayersList(playersList) {
   element1.innerText = "Select an option";
   element2.innerText = "Select an option";
   element1.style.cssText =
-    "background:${color};font-weight:bold;font-size:20px;";
+    "background:#0563af;font-weight:bold;font-size:20px;";
   element2.style.cssText =
-    "background:${color};font-weight:bold;font-size:20px;";
+    "background:#0563af;font-weight:bold;font-size:20px;";
+  playersList1.append(element1);
+  playersList2.append(element2);
+  var element1 = document.createElement("option");
+  var element2 = document.createElement("option");
+  element1.innerText = "BANK";
+  element2.innerText = "BANK";
+  element1.style.cssText =
+    "background:#3d3d3d;font-weight:bold;font-size:20px;";
+  element2.style.cssText =
+    "background:#3d3d3d;font-weight:bold;font-size:20px;";
   playersList1.append(element1);
   playersList2.append(element2);
   console.log(playersList["users"][0]);
   for (var i = 0; i < playersList["users"].length; i++) {
     let color = playersList["users"][i].color;
+    console.log(color);
     var element1 = document.createElement("option");
     var element2 = document.createElement("option");
     element1.innerText = playersList["users"][i].username;
     element2.innerText = playersList["users"][i].username;
     element1.style.cssText =
-      "background:${color};font-weight:bold;font-size:20px;";
+      "background:" + color + ";font-weight:bold;font-size:20px;";
     element2.style.cssText =
-      "background:${color};font-weight:bold;font-size:20px;";
+      "background:" + color + ";font-weight:bold;font-size:20px;";
     playersList1.append(element1);
     playersList2.append(element2);
   }
